c9604e24d7f454981399a6e42a5cf458a6270f2e
Kristen Behrakis <kbehraki8@gmail.com>
2018-11-30 23:39:03 -0500

    removing the hardcoding of special dates and fixing timeout issues

--- back-end/routes/index.js
+++ back-end/routes/index.js
@@ -72,12 +72,14 @@ MongoClient.connect(url, function(err, db) {
   //********** END ADDING COURSE DATA TO DATABASE **********
 
 
-
     var reqClasses = ["SCI1399: Special Topics in Chemistry: Paper Panacea, Part 1"];
-    var semesterInfo = getSemesterInfo()
 
+    var semesterInfo = getSemesterInfo()
+  setTimeout(function(){
+    console.log("RIGHT HERE: " )//+semesterInfo[0].toString())
     // Get the object storing the list of the days we will need to exclude from the iCals and the start dates
     var excludedDaysANDfirstDates = getExludedDatesANDfirstDates(semesterInfo);
+
     var mondayExclusions = excludedDaysANDfirstDates[0].allDates;
     var tuesdayExclusions = excludedDaysANDfirstDates[1].allDates;
     var wednesdayExclusions = excludedDaysANDfirstDates[2].allDates;
@@ -91,8 +93,8 @@ MongoClient.connect(url, function(err, db) {
     var firstThursday = excludedDaysANDfirstDates[3].firstDate;
     var firstFriday = excludedDaysANDfirstDates[4].firstDate;
 
-    // CHANGE TO: const endDate = semesterInfo[1];
-    var endDate = new Date('May 03 2019 00:00:00 UTC')
+    var endDate = formatEventsCalendarTime(semesterInfo[1][0])[0];
+  // var endDate = new Date('May 03 2019 00:00:00 UTC')
 
 
   // ******************** START ICAL SETUP *******************/
@@ -101,7 +103,6 @@ MongoClient.connect(url, function(err, db) {
   // https://www.npmjs.com/package/ical-generator
   const ical = require('ical-generator');
   const http = require('http');
-  //const cal = ical({domain: 'github.com', name: 'my first iCal'});
 
   function icalGen(eventToAdd){
         const cal = ical();
@@ -115,10 +116,8 @@ MongoClient.connect(url, function(err, db) {
             case "mo": excludedDates = excludedDates.concat(mondayExclusions)
             break;
             case "tu": excludedDates = excludedDates.concat(tuesdayExclusions)
-            console.log("tuesday")
             break;
             case "we": excludedDates = excludedDates.concat(wednesdayExclusions)
-            console.log("wedensday")
             break;
             case "th": excludedDates = excludedDates.concat(thursdayExclusions)
             break;
@@ -133,13 +132,13 @@ MongoClient.connect(url, function(err, db) {
             summary: eventToAdd.className,
             repeating: {
               freq: 'WEEKLY',
-              until: endDate,
+              until: moment(endDate),
               exclude: excludedDates,
               byDay: eventToAdd.days,
               }
         });
         return cal
-  }
+    }
 
   // ******************** END ICAL SETUP *******************/
 
@@ -171,12 +170,9 @@ MongoClient.connect(url, function(err, db) {
 
 
 
-  // Need to do a timeout because we need to wait until the info has been fully added to the database
-  setTimeout(function(){
     router.post('/send', (req, res, next) => {
        console.log("RES: "+res)
        extractClasses(reqClasses,convert,res,req)
-
      })
 
 
@@ -266,32 +262,29 @@ MongoClient.connect(url, function(err, db) {
     }
 
     function findRepeatDays(date){
+      var dict = new Object();
+      dict["T"] = "tu";
+      dict["M"] = "mo";
+      dict["W"] = "we";
+      dict["R"] = "th";
+      dict["F"] = "fr";
 
-    var dict = new Object();
-    dict["T"] = "tu";
-    dict["M"] = "mo";
-    dict["W"] = "we";
-    dict["R"] = "th";
-    dict["F"] = "fr";
+      spacePos = date.search(" ");
+      days = date.slice(0,spacePos);
 
-    spacePos = date.search(" ");
-    days = date.slice(0,spacePos);
+      i = 0;
+      var repeatDays = [];
 
-    i = 0;
-
-    var repeatDays = [];
-    while (i<days.length)
-    {
-        day = days.charAt(i);
-        repeatDays.push(dict[day]);
-        i = i + 1;
-    }
-
-    return repeatDays
+      while (i<days.length)
+      {
+          day = days.charAt(i);
+          repeatDays.push(dict[day]);
+          i = i + 1;
+      }
+      return repeatDays
     }
 
     function formatTime(date){
-
         spacePos = date.search(" ");
         days = date.slice(0,spacePos);
 
@@ -328,13 +321,66 @@ MongoClient.connect(url, function(err, db) {
         endTime = "T"+endHour+endMinutes+"00"
 
         return[startTime,endTime]
-
   }
+}, 3000);
 
+  // Parser to format event dates in the database
+  function formatEventsCalendarTime(date){
+    var map= {
+    'January': "01",
+    'February': "02",
+    'March': "03",
+    'April': "04",
+    'May': "05",
+    'June': "06",
+    'July': "07",
+    'August': "08",
+    'September': "09",
+    'October':"10",
+    'November':"11",
+    'December':"12",
+    }
 
-  }, 2000);
+    var firstSpace = date.search(" ")
+    var comma = date.search(",")
+    var firstOBrackets = date.search("\\(")
+    var hyphen = date.search("\\-")
+    var temp = date.slice(hyphen,comma)
+    var secondOBrackets = temp.search("\\(")
 
+    var month = map[date.slice(0,firstSpace)]
 
+    if (parseInt(month,10) <= 5)
+    {
+        var year = "2019"
+    }
+    else{
+          var year = "2018"
+    }
+
+    var endDate = "XX"
+
+    // If there is a hyphen, this is a multi-day event
+    if(hyphen != -1){
+        endDate = date.slice(hyphen+1,secondOBrackets+hyphen)
+    }
+
+    var startDate = date.slice(firstSpace+1, firstOBrackets)
+
+    // The day needs to be 2 characters. The 5th must be 05, for example
+    if(startDate.length == 1){
+        startDate = "0"+startDate
+    }
+
+    // Extracts the event description for use when making the iCals
+    var eventDescription = date.slice(comma+1)
+
+    // Combine the month/day/year into a single string
+    var startDateFormat = year+"-"+month+"-"+startDate
+    var endDateFormat = year+"-"+month+"-"+endDate
+
+    return [startDateFormat, endDateFormat, eventDescription]
+  }
 
   // ******************** HELPER FUNCTIONS **********************/
   // Get the necessary semester information from the database
@@ -342,20 +388,18 @@ MongoClient.connect(url, function(err, db) {
 
 
   function getSemesterInfo(){
-    var startDate = null
+    var startDate = []
     // Populate the startDate variable using databse info
-    dbo.collection("events").find({"description" :"First day of instruction, Full Semester & Session I Courses"}).toArray(function(err, result) {
+    dbo.collection("events").find({"description" :/First day/}).toArray(function(err, result) {
       if (err) throw err;
-      // Result is an array because there are 2 start dates - one for fall and one for spring
-      startDate = result[1].date;
+      startDate.push(result[0].date)
     });
 
-    var endDate = null
+    var endDate = []
     // Populate the endDate variable using database info
-    dbo.collection("events").find({"description" :"Last day of instruction; Last day to withdraw from a Full Session or Session II Course"}).toArray(function(err, result) {
+    dbo.collection("events").find({"description" :/Last day/}).toArray(function(err, result) {
       if (err) throw err;
-      // Result is an array because there are 2 end dates - one for fall and one for spring
-      endDate = result[1].date;
+      endDate.push(result[0].date)
     });
 
     // Extract all Olin Mondays from the database
@@ -380,13 +424,35 @@ MongoClient.connect(url, function(err, db) {
       })
     });
 
-    // Need to wait to make sure we have populated all varaibles assigned above
-    setTimeout(function(){
-      var semesterInfo = [startDate, endDate, olinMondays, noClasses]
-      return semesterInfo
-    }, 2000)
+    var semesterInfo = [startDate, endDate, olinMondays, noClasses]
+    return semesterInfo
   }
 
+ // Helper function for getExludedDatesANDfirstDates. Gets all the dates when there is a school vacation week
+  function checkMultiDay(entry){
+    var days = []
+    // This indicates that it is NOT multi-day event
+    if(entry[1].includes("XX")){
+      days.push(entry[0])
+    }
+    else{
+      var firstDay = moment(entry[0])
+      var lastDay = moment(entry[1])
+
+      // Get all dates between start and end of multi-day off period
+      while (firstDay.isSameOrBefore(lastDay)) {
+        var formattedDate = firstDay.format('YYYY-MM-DD');
+
+        // Add the day to the list
+        days.push(formattedDate)
+
+        // Check the next day
+        firstDay.add(1, 'days');
+      }
+    }
+
+    return days
+  }
 
   // Returns a JSON object that stores the Olin Mondays and days off that occur on each day during the semester
   // This will get us a list of unusual days that we will need to exclude when making the iCals
@@ -404,65 +470,63 @@ MongoClient.connect(url, function(err, db) {
           ....
       ]*/
 
-
   function getExludedDatesANDfirstDates(semesterInfo) {
-    /**** THESE NEED TO BE PARSED CORRECTLY SO WE CAN USE THEM ****/
-    //const startDate = semesterInfo[0];
-    //const endDate = semesterInfo[1];
-    //const olinMondays = semesterInfo[2];
-    //const noClasses = semesterInfo[3];
-    const startDate = "2019-01-23"
-    const endDate = "2019-05-03"
-    const olinMondays = ["2019-02-20", "2019-04-11"]
-    const noClasses = ["2019-02-18","2019-03-07", "2019-03-18", "2019-03-19", "2019-03-20","2019-03-21","2019-03-22", "2019-04-15"]
+      const startDate = formatEventsCalendarTime(semesterInfo[0][0])[0];
+      const endDate = formatEventsCalendarTime(semesterInfo[1][0])[0];
+      const olinMondays = semesterInfo[2].map(x => formatEventsCalendarTime(x)[0]);
 
-    // This will keep track of the days.  0 is Sunday, 1 is Monday, etc.
-    var day = 1
+      // noClasssesNotFlat returns a list of lists so we need to flatten it
+      const noClasssesNotFlat = semesterInfo[3].map(x => checkMultiDay(formatEventsCalendarTime(x)));
 
-    var jsonObject = [{"day":"Monday", "allDates":[], "firstDate":""},
-                      {"day":"Tuesday", "allDates":[], "firstDate":""},
-                      {"day":"Wednesday", "allDates":[], "firstDate":""},
-                      {"day":"Thursday", "allDates":[], "firstDate":""},
-                      {"day":"Friday", "allDates":[], "firstDate":""}
-                    ]
+      // OlinMondaysNotFlat returns a list of lists so we need to flatten it
+      const noClasses = [].concat.apply([], noClasssesNotFlat);
 
-    // For each day of the week, we will be adding dates
-    jsonObject.forEach(function(weekday) {
-      var result = [];
-      var current = moment(startDate);
-      var count = 0;
+      // This will keep track of the days.  0 is Sunday, 1 is Monday, etc.
+      var day = 1
 
-      // While we haven't reached the end date, keep adding days
-      while (current.day(day).isSameOrBefore(endDate)) {
-        var formattedDate = current.format('YYYY-MM-DD');
-        var isUnusualDate = olinMondays.includes(formattedDate) || noClasses.includes(formattedDate)
+      var jsonObject = [{"day":"Monday", "allDates":[], "firstDate":""},
+                        {"day":"Tuesday", "allDates":[], "firstDate":""},
+                        {"day":"Wednesday", "allDates":[], "firstDate":""},
+                        {"day":"Thursday", "allDates":[], "firstDate":""},
+                        {"day":"Friday", "allDates":[], "firstDate":""}
+                      ]
 
-        if(current.day(day).isSameOrAfter(startDate)){
-          // If the day we're adding is a day with no classes or an Olin monday, then we will add it to the meeting times we need to exlucde for this day
-          if(isUnusualDate){
-            result.push(formattedDate);
+      // For each day of the week, we will be adding dates
+      jsonObject.forEach(function(weekday) {
+        var result = [];
+        var current = moment(startDate);
+        var count = 0;
+
+        // While we haven't reached the end date, keep adding days
+        while (current.day(day).isSameOrBefore(endDate)) {
+          var formattedDate = current.format('YYYY-MM-DD');
+          var isUnusualDate = olinMondays.includes(formattedDate) || noClasses.includes(formattedDate)
+
+          if(current.day(day).isSameOrAfter(startDate)){
+            // If the day we're adding is a day with no classes or an Olin monday, then we will add it to the meeting times we need to exlucde for this day
+            if(isUnusualDate){
+              result.push(formattedDate);
+            }
+            // If this is the first weekday of the semester, add it to the json object
+            else if(count == 0){
+              weekday.firstDate = formattedDate
+              count = count + 1;
+            }
           }
-          // If this is the first weekday of the semester, add it to the json object
-          else if(count == 0){
-            weekday.firstDate = formattedDate
-            count = count + 1;
-          }
+
+          // Skip forward 1 week to get the next date
+          current.day(7)
         }
 
-        // Skip forward 1 week to get the next date
-        current.day(7)
-      }
+        weekday.allDates = result
 
-      weekday.allDates = result
+        // Move ahead one day
+        day = day + 1
+      });
 
-      // Move ahead one day
-      day = day + 1
-    });
-
-    //console.log(jsonObject);
-    return jsonObject;
-  };
-
+      console.log(jsonObject);
+      return jsonObject;
+  }
 });
 
 module.exports = router;
